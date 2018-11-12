28dc326ad634bfbcc0a5fe0d5dbf03090261bc40
cmateo10 <2015081590@ust-ics.mygbiz.com>
2018-11-12 23:59:04 +0800

    Initial commit

---
+++ app/src/main/java/com/healthy/happy/happyhealthy/CreateAccount.java
@@ -0,0 +1,20 @@
+package com.healthy.happy.happyhealthy;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CreateAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+
+    public void Back(View v)  {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+}
